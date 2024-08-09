42682b49fce17209ebb4ef6c21e9f2b13159314b
alex1zz <diegocholotio@emilianisomascos.edu.gt>
2024-08-09 10:58:32 -0600

    Implementa el controlador y servicio de tu API REST

--- Proyecto_Integrador-main/src/main/java/Ada_School/Proyecto_Integrador/ProyectoIntegradorApplication.java
+++ Proyecto_Integrador-main/src/main/java/Ada_School/Proyecto_Integrador/ProyectoIntegradorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoIntegradorApplication {
+public class gitProyectoIntegradorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoIntegradorApplication.class, args);
